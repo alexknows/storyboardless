5dfe03d930f1dba6d714b6c2bf54a3090b361302
Alex Cruz <cruzbiz11@gmail.com>
2019-11-14 13:39:59 -0500

    First commit

---
+++ storyboardless/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyboardless
+//
+//  Created by Alex Cruz on 11/14/19.
+//  Copyright © 2019 Alex Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
